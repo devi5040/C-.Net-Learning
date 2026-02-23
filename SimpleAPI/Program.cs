c47692baaa3ad9fa8d0b01fb9fba965b0f240c70
devi5040 <dpraidola@gmail.com>
2026-02-23 19:18:08 +0530

    feat: Implement initial setup for SimpleAPI with service registration and HTTP request pipeline configuration

---
+++ SimpleAPI/Program.cs
@@ -0,0 +1,24 @@
+using SimpleApi.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IUserService, UserService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.MapControllers();
+app.UseHttpsRedirection();
+
+
+
+app.Run();
