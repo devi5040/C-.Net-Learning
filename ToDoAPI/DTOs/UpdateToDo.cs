2e068d8d40a4392941f0727150ff639b7e4e25c3
devi5040 <dpraidola@gmail.com>
2026-02-24 18:35:38 +0530

    feat: Add UpdateToDoDto for updating ToDo item details

---
+++ ToDoAPI/DTOs/UpdateToDo.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace TodoApi.DTOs;
+
+public class UpdateToDoDto
+{
+    [Required(ErrorMessage = "Title is required.")]
+    [StringLength(100, MinimumLength = 3, ErrorMessage = "Title must be between 3 and 100 characters.")]
+    public string Title { get; set; } = string.Empty;
+
+    [StringLength(500, ErrorMessage = "Description cannot exceed 500 characters.")]
+    public string? Description { get; set; }
+
+    public bool IsCompleted { get; set; }
+}
