7efee3a94797c86b6725a1ccaa965050753c5b56
devi5040 <dpraidola@gmail.com>
2026-02-24 18:33:02 +0530

    feat: Initialize Program.cs with basic setup for web application and services

---
+++ ToDoAPI/Program.cs
@@ -0,0 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Data;
+using TodoApi.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseInMemoryDatabase("TodoDb"));
+builder.Services.AddScoped<ITodoService, TodoService>();
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+
+app.Run();
+
