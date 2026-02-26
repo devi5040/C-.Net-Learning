57e11db34d09db21bae614f75e508a80ba110b93
devi5040 <dpraidola@gmail.com>
2026-02-26 19:01:15 +0530

    feat: Configure core services and middleware. Adds DbContext, IUrlShortenerService, IAnalyticsService, controllers, Swagger, exception handling middleware, and maps controllers.

--- URLShortner/Program.cs
+++ URLShortner/Program.cs
@@ -1,9 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using UrlShortener.Data;
+using UrlShortener.Middleware;
+using UrlShortener.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
+// services
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseInMemoryDatabase("UrlShortenerDb"));
+builder.Services.AddScoped<IUrlShortenerService, UrlShortenerService>();
+builder.Services.AddScoped<IAnalyticsService, AnalyticsService>();
+builder.Services.AddControllers();
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "URL Shortener API", Version = "v1" });
+});
 
 var app = builder.Build();
+
+app.UseMiddleware<ExceptionHandlingMiddleware>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -13,5 +30,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
 
 app.Run();
